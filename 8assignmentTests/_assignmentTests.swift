1b600d310b0d4918f13af21e6e6b3d03c08706a5
Ablikon <ablik2005@gmail.com>
2025-11-21 08:35:01 +0500

    Initial Commit

---
+++ 8assignmentTests/_assignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  _assignmentTests.swift
+//  8assignmentTests
+//
+//  Created by Абылайхан Бегимкулов on 21.11.2025.
+//
+
+import Testing
+@testable import _assignment
+
+struct _assignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
